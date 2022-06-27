1fe374ddc7909739df92ca1427c6a99db8b1b32b
Anna Radchenko <anrd.rs>
2022-06-27 19:01:01 +0300

    ComicsPage component updated

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,8 +5,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
+
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
