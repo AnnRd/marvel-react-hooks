14fb69232970ca1fda7d1b7235f6dd32ab7b9979
Anna Radchenko <anrd.rs>
2022-06-28 18:20:27 +0300

    appHeader updated

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,31 +1,17 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import { MainPage, ComicsPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-    
-
     return (
         <Router>
             <div className="app">
                 <AppHeader/>
                 <main> 
                     <Routes>
-                    <Route path="/">
-                        
-                    </Route>
-                    <Route path="/comics">
-                        <AppBanner/>
-                        <ComicsList/>
-                    </Route>
+                        <Route path="/" element={<MainPage/>}/> 
+                        <Route path="/comics" element={<ComicsPage/>}/>
                     </Routes>
                 </main>
             </div>
